c1d69a57f4b5bd6d0a6a4b2f0eb64377eab8a6fd
suvedo <764385337@qq.com>
2025-04-08 11:01:47 +0800

    add opensource Kolors

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="kolors",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        # 这里可以添加你的依赖包
+    ],
+    python_requires=">=3.6",
+) 
